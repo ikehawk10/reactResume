c5595061ac9d8a7dd31b8b4cd96c76e059da844b
Isaac <hawkins.isaac@gmail.com>
2017-08-17 16:41:53 -0500

    replace static resume section

--- resume/src/App.js
+++ resume/src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
-        <Resume />
+        <Resume data={this.state.resumeData.resume}/>
         <Portfolio />
         <Testimonials />
         <Contact />
